129661cd4eb0c13003ed9a643848e0ba40625ed3
Lily Sijia Li <lilysijiali@gmail.com>
2025-09-04 11:58:36 +0200

    Initial data_loader and visualisation

---
+++ README.md
@@ -0,0 +1,2 @@
+# ddos-regression-analysis
+Linear regression model to predict DDoS attacks. Model is built on intrusion detection evaluation dataset (CIC-IDS2017) by Canadian Institute for Cybersecurity.
---
+++ src/data_loader.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def uni_load_flow_data(csv_path):
+    """
+    Loads all columns from rows labeled 'DDoS'.
+    """
+    df = pd.read_csv(csv_path)
+    df.columns = df.columns.str.strip()
+    df_ddos = df[df['Label'] == 'DDoS']
+    return df_ddos
+
